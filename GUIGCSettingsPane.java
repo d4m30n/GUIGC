fdee8a9c78b7076e9010f7a0479d6e3b5d4274ac
d4m30n <w0455rk@protonmail.com>
2018-12-12 14:47:17 +1300

    Adds new settings and random string and number generator

--- GUIGCSettingsPane.java
+++ GUIGCSettingsPane.java
@@ -6,15 +6,69 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.geometry.Pos;
 import javafx.event.ActionEvent;
+import java.utils.Random;
 
 public class GUIGCSettingsPane extends HBox{
 	
+	final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	final int MIN_CHAR = 1;
+	final int MAX_CHAR = 50;
 	TextField repsField = new TextField("1000");
 	TextField seedField = new TextField("438");
 	TextField depthField = new TextField("8");
 	TextField breadthField = new TextField("2");
 	TextField nButtonsField = new TextField("1");
 	TextField sleepTimeField = new TextField("100");
+
+	private int hashTimes = 0; // Number of times to hash the string.
+	private int randomDelete = 0; // The percentage of objects to delete.
+	private Random rand = null; // The random number generator.
+
+	/**
+	 * AUTHOR: Harry McCarthy
+	 * Simple methord that checks if the random number generator has been set
+	 * Need to check this when the program is running so cant set it when the
+	 * object is created due to needing the seed.
+	 */
+	public void randCheck(){
+		if(rand == null){
+			rand = new Random(getSeed());
+		}
+	}
+
+	/**
+	 * AUTHOR: Harry McCarthy
+	 * Generates a random number from the seed in the settings.
+	 * @param low The lowist number in the random range.
+	 * @param high The highest number in the random range.
+	 * @return Returns a random number between the range give.
+	 */
+	public int randomNumber(int low, int high){
+		randCheck();
+		return rand.nextInt((high-low)+1)+low;
+	}
+
+	/**
+	 * AUTHOR: Harry McCarthy
+	 * Generate a random string on the lenght defined above.
+	 * @return A random String
+	 */
+	public String randomString(){
+		randCheck();
+		String returnString = "";
+		int numChars = randomNumber(MIN_CHAR,MAX_CHAR);
+		for(int place = 0; place < numChars; place++){
+			returnString += ALPHABET.charAt(randomNumber(0, ALPHABET.length()-1));
+		}
+		return returnString;
+	}
+
+	public int NumHash(){
+		return this.hashTimes;
+	}
+	public int numDelete(){
+		return this.randomDelete;
+	}
 	
 	Button buttonA, buttonB, buttonC, buttonD;
 	
